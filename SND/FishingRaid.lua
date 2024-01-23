01bd6cf3627da4d6cc0f71e5e6617c1b6921672c
plottingCreeper <plottingCreeper@gmail.com>
2024-01-24 11:56:55 +1300

    Update FishingRaid.lua

--- SND/FishingRaid.lua
+++ SND/FishingRaid.lua
@@ -1,7 +1,10 @@
 --[[
   Ocean fishing spam, using AutoHook, Pandora, and Visland
-  Some bits borrowed and/or stolen from Curio Salso's feeshing script
 ]]
+
+ar_while_waiting = true --AutoRetainer multimode enabled in between fishing trips.
+fishing_character = "auto" --"auto" requires starting the script while on your fishing character.
+adjust_z = true --true might cause stuttery movement, false might cause infinite movement. Good luck.
 
 start_fishing = {
   "/wait 0.1",
@@ -11,32 +14,58 @@ start_fishing = {
 
 back_in_limsa = {
   "/wait 5",
+  "/discardall",
+  "/wait 5",
 }
 
-countdown_timer = false
-adjust_z = true
+bags_full = {
+  LeaveDuty(),
+  "/pcraft stop",
+}
+
+------------------------------------------------------------------------
 
 ::Start::
-if IsInZone(900) then goto OnBoat end
+if IsInZone(900) then
+  goto OnBoat
+elseif IsInZone(129) and GetDistanceToPoint(-410,4,76)<6.9 then
+  if GetCharacterCondition(91) then
+    goto Enter
+  elseif os.date("!*t").hour%2==0 and os.date("!*t").min<15 then
+    goto Queue
+  elseif os.date("!*t").hour%2==1 and os.date("!*t").min>=45 then
+    goto WaitForBoat
+  else
+    goto StartAR
+  end
+elseif fishing_character ~= "auto" then
+  goto ARWait
+else
+  yield("/echo That's not gonna work, chief.")
+  yield("/pcraft stop")
+end
+
+::ARWait::
+if ar_while_waiting then
+  while not ( os.date("!*t").hour%2==1 and os.date("!*t").min>=55 ) do
+    yield("/wait 1.001")
+  end
+  yield("/ays multi")
+  while GetCharacterName(true)~=fishing_character do
+    if IsAddonVisible("TitleConnect") or IsAddonVisible("NowLoading") or IsAddonVisible("CharaSelect") then
+      yield("/wait 1.002")
+    elseif GetCharacterCondition(50,false) then
+      yield("/ays relog " .. fishing_character)
+    elseif IsAddonVisible("RetainerList") then
+      yield("/pcall RetainerList true -1")
+    end
+    yield("/wait 1.009")
+  end
+end
 
 ::WaitForBoat::
-elapsed = os.time()
-while elapsed > 7200 do
-  elapsed = elapsed - 7200
-end
-last_boat = os.time() - elapsed
-
-wait_time = math.floor(7200-os.difftime(os.time(), last_boat))
-while wait_time >= 1 do
-  wait_time = math.floor(7200-os.difftime(os.time(), last_boat))
-  if type(countdown_timer)=="number" then
-    if wait_time % countdown_timer == 0 then
-      minutes = wait_time // 60
-      seconds = wait_time % 60
-      yield("/echo Waiting "..minutes.." minutes and "..seconds.." seconds for the next boat")
-    end
-  end
-  yield("/wait 1.0")
+while not ( os.date("!*t").hour%2==0 and os.date("!*t").min<15 ) do
+  yield("/wait 1.010")
 end
 
 ::BotPause::
@@ -46,7 +75,7 @@ yield("/wait "..notabot)
 
 ::Queue::
 while GetCharacterCondition(91, false) do
-  if GetTargetName()=="" then
+  if GetTargetName()~="Dryskthota" or IsAddonVisible("_TargetInfoMainTarget")==false then
     yield("/target Dryskthota")
   elseif GetCharacterCondition(32, false) then
     yield("/pinteract")
@@ -63,44 +92,48 @@ end
 ::Enter::
 while IsInZone(129) do
   if IsAddonVisible("ContentsFinderConfirm") then yield("/pcall ContentsFinderConfirm true 8") end
-  yield("/wait 1.01")
+  yield("/wait 1.020")
 end
-while GetCharacterCondition(35, false) do yield("/wait 1.02") end
+while GetCharacterCondition(35, false) do yield("/wait 1.021") end
 
 ::PrepareRandom::
 is_entering = true
-random_x = math.random(1,2)
-if random_x == 1 then move_x = 7.5
+if tostring(math.random(2))=="1" then move_x = 7.5
 else move_x = -7.5 end
 move_y = math.random(-11000,5000)/1000
 move_z = 6.750
 
 ::OnBoat::
-while not IsInZone(129) do
+while IsInZone(900) do
   if IsAddonVisible("IKDFishingLog") then
-    yield("/wait 0.11")
+    yield("/wait 0.101")
     timer_string = string.gsub(GetNodeText("IKDFishingLog", 18),"%D","")
-    while timer_string=="" do
-      timer_string = string.gsub(GetNodeText("IKDFishingLog", 18),"%D","")
-      yield("/wait 0.111")
-    end
     timer = tonumber(timer_string)
-    while timer < 030 or timer > 698 do
-      yield("/wait 1.05")
-      timer_string = string.gsub(GetNodeText("IKDFishingLog", 18),"%D","")
-      while timer_string=="" do
+    if type(timer)=="number" then
+      while timer < 030 or timer > 698 do
+        yield("/wait 1.031")
         timer_string = string.gsub(GetNodeText("IKDFishingLog", 18),"%D","")
-        yield("/wait 0.112")
+        while timer_string=="" do
+          timer_string = string.gsub(GetNodeText("IKDFishingLog", 18),"%D","")
+          yield("/wait 0.103")
+        end
+        timer = tonumber(timer_string)
+        if IsAddonVisible("IKDFishingLog")==false then timer = 500 end
       end
-      timer = tonumber(timer_string)
-      if IsAddonVisible("IKDFishingLog")==false then timer = 500 end
     end
     if is_entering then
       is_move = true
       is_entering = false
     end
   end
-  if GetCharacterCondition(35) then
+  if IsAddonVisible("_TextError") then
+    if GetNodeText("_TextError", 1)=="Unable to gather. Insufficient inventory space." then
+      for _, command in pairs(bags_full) do
+        yield("/echo Running: "..command)
+        yield(command)
+      end
+    end
+  elseif GetCharacterCondition(35) then
     while GetCharacterCondition(35) do yield("/wait 1.03") end
     yield("/wait 3")
   elseif IsAddonVisible("NowLoading") then
@@ -120,7 +153,7 @@ while not IsInZone(129) do
       end
       yield("/wait 0.1")
     end
-    if random_x == 1 then yield("/visland moveto 9 "..move_z.." "..move_y)
+    if move_x == 7.5 then yield("/visland moveto 9 "..move_z.." "..move_y)
     else yield("/visland moveto -9 "..move_z.." "..move_y) end
     yield("/wait 0.2")
     yield("/visland stop")
@@ -131,12 +164,24 @@ while not IsInZone(129) do
       yield(command)
     end
   end
-  yield("/wait 1.09")
+  yield("/wait 1.039")
 end
 
 for _, command in pairs(back_in_limsa) do
   yield("/echo Running: "..command)
   yield(command)
+end
+
+::StartAR::
+if ar_while_waiting then
+  if fishing_character=="auto" then fishing_character = GetCharacterName(true) end
+  yield("/ays multi")
+  while GetCharacterCondition(1) do
+    yield("/wait 1")
+  end
+  goto ARWait
+else
+  goto WaitForBoat
 end
 
 GetNodeText("_BattleTalk", 4)
