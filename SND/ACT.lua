3a7a970eebc33292f2648bb082ab7fc14d6acfc1
plottingCreeper <plottingCreeper@gmail.com>
2024-01-26 04:51:11 +1300

    Create ACT.lua

---
+++ SND/ACT.lua
@@ -0,0 +1,13 @@
+--[[
+  Nothing to see here, move along.
+  Just some Lua to start ACT for no reason at all.
+]]
+
+os.execute("start \"ACT\" /D \""..os.getenv("ProgramFiles(x86)").."\\Advanced Combat Tracker\\\" \"Advanced Combat Tracker.exe\"")
+
+--[[ 
+  Yes I know it can be a LOT simpler, and probably just as reliable. 
+  I don't know anyone who moves their program files, and frankly I don't want to.
+  Does winblows even care about starting directory anymore? Pretty sure that's a holdover from DOS.
+  Either way, shut up. It's fine.
+]]
